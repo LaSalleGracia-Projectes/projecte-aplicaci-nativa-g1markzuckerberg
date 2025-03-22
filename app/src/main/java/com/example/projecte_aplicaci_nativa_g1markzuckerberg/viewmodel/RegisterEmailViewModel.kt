60511a0d613faa3556358db35ce4f4e49c995944
albertgarrido4 <algarri2000@gmail.com>
2025-03-22 18:23:45 +0100

    feat: Create RegisterEmailViewModel

---
+++ app/src/main/java/com/example/projecte_aplicaci_nativa_g1markzuckerberg/viewmodel/RegisterEmailViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.projecte_aplicaci_nativa_g1markzuckerberg.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class RegisterEmailViewModel: ViewModel() {
+    //TODO: Login logic
+}
